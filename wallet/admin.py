2bc3c0ce64e8204d5f36fd47d3c664716ea4c339
gathangamaryann <maryanngathanga@gmail.com>
2022-08-01 22:19:37 +0300

    first commit

---
+++ wallet/admin.py
@@ -0,0 +1,28 @@
+import email
+from locale import currency
+from django.contrib import admin 
+
+
+
+
+from.models import Account, Chatbot, Currency, Customer, Loan, Notifications, Qr_code, Receipt, Reward, Third_party, Transaction, Wallet
+
+
+
+# # Register your models here.
+# class CustomerAdmin(admin.ModelAdmin):
+#   list_display=('first_name','Last_name','age','email',)
+#   search_field=('first_name','last_name')
+
+admin.site.register(Customer)
+admin.site.register(Account)
+admin.site.register(Loan)
+admin.site.register(Chatbot)
+admin.site.register(Notifications)
+admin.site.register(Currency)
+admin.site.register(Wallet)
+admin.site.register(Qr_code)
+admin.site.register(Receipt)
+admin.site.register(Reward)
+admin.site.register(Third_party)
+admin.site.register(Transaction)
---
+++ wallet/models.py
@@ -0,0 +1,113 @@
+from datetime import datetime
+from distutils.command.upload import upload
+from importlib.util import module_from_spec
+from time import time
+from django.db import models
+
+
+import wallet
+
+
+
+# Create your models here.
+class Customer(models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    adress=models.TextField()
+    email=models.EmailField()
+    phone_number=models.CharField(max_length=15)
+    gender=models.CharField(max_length=10,null=True)
+    age=models.CharField(max_length=10)
+    # GENDER_CHOICES=(
+    #     ('m','Male'),
+    #      ('f','Female')
+    #  )
+
+    # gender=models.CharField(max_length=6,choices=GENDER_CHOICES,null=True)
+    profile_picture=models.ImageField(upload_to='profile_picture/',null=True)
+class Account(models.Model):   
+    account_name=models.CharField(max_length=20)
+    account_number=models.CharField(max_length=20)
+    account_pin=models.PositiveBigIntegerField()
+    account_balance=models.PositiveBigIntegerField()
+class Loan(models.Model): 
+    amount=models.PositiveBigIntegerField()
+    period_of_loan=models.DateTimeField()
+    repayment_reschedules=models.DateTimeField()
+    loan_id=models.CharField(max_length=20) 
+    loan_type=models.CharField(max_length=20) 
+class Chatbot(models.Model):
+    support_and_agent_rating=models.CharField(max_length=20)
+    transcript_emails=models.CharField(max_length=100)
+    chat_log=models.CharField(max_length=20)
+class Notifications(models.Model):
+    user_id=models.PositiveBigIntegerField()
+    message= models.CharField(max_length=100)
+    transaction_description=models.CharField(max_length=100)
+    transaction_id=models.CharField(max_length=20)  
+    transaction_amount=models.PositiveBigIntegerField() 
+    transaction_number=models.CharField(max_length=100)
+class Currency(models.Model):
+    country_of_origin=models.CharField(max_length=100) 
+    symbol=models.CharField(max_length=100)
+    amount=models.PositiveBigIntegerField()  
+class Wallet(models.Model):
+    balance=models.PositiveBigIntegerField()
+    user_id=models.PositiveBigIntegerField() 
+    amount=models.PositiveBigIntegerField() 
+    time=models.DateTimeField()
+    status=models.CharField(max_length=100)
+    history=models.DateTimeField(default=datetime.now)
+class Qr_code(models.Model):
+    website_url=models.URLField() 
+    contact_details=models.EmailField()   
+class Receipt(models.Model):
+    receipt_type=models.CharField(max_length=100)   
+    receipt_date=models.DateField()
+    bill_number=models.PositiveBigIntegerField()
+class Reward(models.Model):
+    name=models.CharField(max_length=20)   
+    customer_id=models.PositiveBigIntegerField() 
+    gender=models.CharField(max_length=20)
+class Third_party(models.Model):
+    account_name=models.CharField(max_length=20)
+    account_number=models.PositiveBigIntegerField()
+    amount=models.PositiveBigIntegerField()
+    currency=models.CharField(max_length=20)
+    location=models.CharField(max_length=20)
+class Transaction(models.Model):
+    transaction_Code=models.CharField(max_length=20)
+    transaction_number=models.CharField(max_length=20)
+    transaction_type=models.CharField(max_length=20)
+    transaction_charge=models.BigIntegerField
+    date_and_Time=models.DateTimeField
+    wallet=models.ForeignKey(null=True,on_delete=models.CASCADE,to=Wallet)
+    receipt=models.OneToOneField(Receipt,on_delete=models.CASCADE,primary_key=True)
+    origin_account= models.ForeignKey(Account,on_delete=models.CASCADE,related_name="Transaction_origin_account",null=True)
+    destination_account=models.ForeignKey(Account,on_delete=models.CASCADE,related_name="Transaction_destination_account",null=True)
+class Card(models.Model):
+    user_Card_number=models.IntegerField()
+    pin=models.CharField(max_length=20)
+    debit_card=models.CharField(max_length=20)
+    credit_card=models.CharField(max_length=20)
+    expiry_date=models.DateTimeField(default=datetime.now)
+    issued_date=models.DateTimeField(default=datetime.now)
+    wallet=models.ForeignKey(Account,on_delete=models.CASCADE,related_name="Card_wallet")
+    account=models.ForeignKey(Account,on_delete=models.CASCADE,related_name="Card_account")
+    issuer=models.CharField(max_length=20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
